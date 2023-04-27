10b3bb289035634335bc611c244d942c573dc387
Jurakii <102999225+Jurakii@users.noreply.github.com>
2023-04-27 00:09:21 -0700

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,122 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "rmav99EE,":
+            case "MenuButton":return img`
+. . . . . c c c c c . . . . . 
+. . . c c 1 1 1 1 1 c c . . . 
+. . c 1 1 d d d d d 1 1 c . . 
+. c 1 d d d d d d d d d 1 c . 
+. c 1 d d d 1 1 1 d d d 1 c . 
+c 1 d d d 1 d d d 1 d d b 1 c 
+c 1 d d d 1 d d d 1 d d b 1 c 
+c 1 d d d 1 1 1 1 1 d d b 1 c 
+c 1 d d d 1 d d d 1 d d b 1 c 
+c 1 d d d 1 d d d 1 d b b 1 c 
+. c 1 d d d d d d d d b 1 c . 
+. c 1 d d d d d d d b b 1 c . 
+. . c 1 1 b b b b b 1 1 c . . 
+. . . c c 1 1 1 1 1 c c . . . 
+. . . . . c c c c c . . . . . 
+`;
+            case "WHMG?%h~ePI9)!p{{}1R":
+            case "Title":return img`
+..1111111111111111111111................
+.1cccc111ccc11c11111c111................
+.1c111c1c111c1c11111c1111...............
+.1c111c1c111c1c11111c1111...............
+.1cccc11ccccc1c11111c1111...............
+.1c111c1c111c1c11111c1111...............
+.1c111c1c111c1c11111c1111...............
+.1cccc11c111c1cccc11cccc1...............
+.111111111111111111111111111111111111...
+.1cccc111ccc11c111c1c111c11cccc1ccccc1..
+.1c111c1c111c1c111c1cc11c1c11111c11111..
+.1c111c1c111c1c111c1c1c1c1c11111c11111..
+.1cccc11c111c1c111c1c11cc1c11111ccccc1..
+.1c111c1c111c1c111c1c111c1c11111c11111..
+.1c111c1c111c1c111c1c111c1c11111c11111..
+.1cccc111ccc111ccc11c111c11cccc1ccccc1..
+..11111111111111111111111111111111111...
+`;
+            case "H-~J_(`+U6)}w(eyXIk(":
+            case "Platform":return img`
+. . c c c c c c c c c c c . . 
+. c 1 1 1 1 1 1 1 1 1 1 1 c . 
+c d d d d d d d d d d d d d c 
+. c b b b b b b b b b b b c . 
+. . c c c c c c c c c c c . . 
+`;
+            case "h`Zx-7Pm0pT9iKXHRhW%":
+            case "Platfor":return img`
+. . c c c c c c c c c c c . . 
+. c 1 1 1 1 1 1 1 1 1 1 1 c . 
+c d d d d d d d d d d d d d c 
+. c b b b b b b b b b b b c . 
+. . c c c c c c c c c c c . . 
+`;
+            case "wXUU6f1=+D;a?rXd(1uJ":
+            case "Ball":return img`
+. . c c c . . 
+. c d d 1 c . 
+c b d d d 1 c 
+c b d d d d c 
+c b b d d d c 
+. c b b b c . 
+. . c c c . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "A Button":
+            case ",(~/eygnF|d:OR}BWxM?":return [img`
+. . . . . c c c c c . . . . . 
+. . . c c 1 1 1 1 1 c c . . . 
+. . c 1 1 d d d d d 1 1 c . . 
+. c 1 d d d d d d d d d 1 c . 
+. c 1 d d d 1 1 1 d d d 1 c . 
+c 1 d d d 1 d d d 1 d d b 1 c 
+c 1 d d d 1 d d d 1 d d b 1 c 
+c 1 d d d 1 1 1 1 1 d d b 1 c 
+c 1 d d d 1 d d d 1 d d b 1 c 
+c 1 d d d 1 d d d 1 d b b 1 c 
+. c 1 d d d d d d d d b 1 c . 
+. c 1 d d d d d d d b b 1 c . 
+. . c 1 1 b b b b b 1 1 c . . 
+. . . c c 1 1 1 1 1 c c . . . 
+. . . . . c c c c c . . . . . 
+`, img`
+. . . . . c c c c c . . . . . 
+. . . c c 1 1 1 1 1 c c . . . 
+. . c 1 1 b b b b b 1 1 c . . 
+. c 1 b b b b b b b b b 1 c . 
+. c 1 b b b 1 1 1 b b b 1 c . 
+c 1 b b b 1 b b b 1 b b c 1 c 
+c 1 b b b 1 b b b 1 b b c 1 c 
+c 1 b b b 1 1 1 1 1 b b c 1 c 
+c 1 b b b 1 b b b 1 b b c 1 c 
+c 1 b b b 1 b b b 1 b c c 1 c 
+. c 1 b b b b b b b b c 1 c . 
+. c 1 b b b b b b b c c 1 c . 
+. . c 1 1 c c c c c 1 1 c . . 
+. . . c c 1 1 1 1 1 c c . . . 
+. . . . . c c c c c . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -7,26 +7,12 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         info.setScore(0)
         sprites.destroy(startButton)
         gameStart += 1
-        Ball = sprites.create(img`
-            . . c c c . . 
-            . c d d 1 c . 
-            c b d d d 1 c 
-            c b d d d d c 
-            c b b d d d c 
-            . c b b b c . 
-            . . c c c . . 
-            `, SpriteKind.Player)
+        Ball = sprites.create(assets.image`Ball`, SpriteKind.Player)
         controller.moveSprite(Ball, 55, 0)
         currentPos = 110
         platformY = 110
         platformX = 0
-        Platforms = sprites.create(img`
-            . . c c c c c c c c c c c . . 
-            . c 1 1 1 1 1 1 1 1 1 1 1 c . 
-            c d d d d d d d d d d d d d c 
-            . c b b b b b b b b b b b c . 
-            . . c c c c c c c c c c c . . 
-            `, SpriteKind.Platform)
+        Platforms = sprites.create(assets.image`Platfor`, SpriteKind.Platform)
         Platforms.setPosition(80, platformY)
         lastX = Platforms.x
     }
@@ -38,13 +24,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Platform, function (sprite, othe
         scene.cameraShake(3, 200)
         music.play(music.createSoundEffect(WaveShape.Triangle, 1, 1809, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
         sprites.destroy(otherSprite, effects.disintegrate, 2000)
-        Platforms = sprites.create(img`
-            . . c c c c c c c c c c c . . 
-            . c 1 1 1 1 1 1 1 1 1 1 1 c . 
-            c d d d d d d d d d d d d d c 
-            . c b b b b b b b b b b b c . 
-            . . c c c c c c c c c c c . . 
-            `, SpriteKind.Platform)
+        Platforms = sprites.create(assets.image`Platform`, SpriteKind.Platform)
         platformY += -50
         currentPos = platformY
         platformX = randint(10, 40)
@@ -68,81 +48,15 @@ let gameStart = 0
 scene.setBackgroundColor(12)
 effects.starField.startScreenEffect()
 gameStart = 0
-let title = sprites.create(img`
-    ..1111111111111111111111................
-    .1cccc111ccc11c11111c111................
-    .1c111c1c111c1c11111c1111...............
-    .1c111c1c111c1c11111c1111...............
-    .1cccc11ccccc1c11111c1111...............
-    .1c111c1c111c1c11111c1111...............
-    .1c111c1c111c1c11111c1111...............
-    .1cccc11c111c1cccc11cccc1...............
-    .111111111111111111111111111111111111...
-    .1cccc111ccc11c111c1c111c11cccc1ccccc1..
-    .1c111c1c111c1c111c1cc11c1c11111c11111..
-    .1c111c1c111c1c111c1c1c1c1c11111c11111..
-    .1cccc11c111c1c111c1c11cc1c11111ccccc1..
-    .1c111c1c111c1c111c1c111c1c11111c11111..
-    .1c111c1c111c1c111c1c111c1c11111c11111..
-    .1cccc111ccc111ccc11c111c11cccc1ccccc1..
-    ..11111111111111111111111111111111111...
-    `, SpriteKind.Title)
-startButton = sprites.create(img`
-    . . . . . c c c c c . . . . . 
-    . . . c c 1 1 1 1 1 c c . . . 
-    . . c 1 1 d d d d d 1 1 c . . 
-    . c 1 d d d d d d d d d 1 c . 
-    . c 1 d d d 1 1 1 d d d 1 c . 
-    c 1 d d d 1 d d d 1 d d b 1 c 
-    c 1 d d d 1 d d d 1 d d b 1 c 
-    c 1 d d d 1 1 1 1 1 d d b 1 c 
-    c 1 d d d 1 d d d 1 d d b 1 c 
-    c 1 d d d 1 d d d 1 d b b 1 c 
-    . c 1 d d d d d d d d b 1 c . 
-    . c 1 d d d d d d d b b 1 c . 
-    . . c 1 1 b b b b b 1 1 c . . 
-    . . . c c 1 1 1 1 1 c c . . . 
-    . . . . . c c c c c . . . . . 
-    `, SpriteKind.Title)
+let title = sprites.create(assets.image`Title`, SpriteKind.Title)
+startButton = sprites.create(assets.image`MenuButton`, SpriteKind.Title)
 startButton.setScale(2, ScaleAnchor.Middle)
 startButton.setPosition(80, 100)
 title.setPosition(80, 45)
 title.setScale(2, ScaleAnchor.Middle)
 animation.runImageAnimation(
 startButton,
-[img`
-    . . . . . c c c c c . . . . . 
-    . . . c c 1 1 1 1 1 c c . . . 
-    . . c 1 1 d d d d d 1 1 c . . 
-    . c 1 d d d d d d d d d 1 c . 
-    . c 1 d d d 1 1 1 d d d 1 c . 
-    c 1 d d d 1 d d d 1 d d b 1 c 
-    c 1 d d d 1 d d d 1 d d b 1 c 
-    c 1 d d d 1 1 1 1 1 d d b 1 c 
-    c 1 d d d 1 d d d 1 d d b 1 c 
-    c 1 d d d 1 d d d 1 d b b 1 c 
-    . c 1 d d d d d d d d b 1 c . 
-    . c 1 d d d d d d d b b 1 c . 
-    . . c 1 1 b b b b b 1 1 c . . 
-    . . . c c 1 1 1 1 1 c c . . . 
-    . . . . . c c c c c . . . . . 
-    `,img`
-    . . . . . c c c c c . . . . . 
-    . . . c c 1 1 1 1 1 c c . . . 
-    . . c 1 1 b b b b b 1 1 c . . 
-    . c 1 b b b b b b b b b 1 c . 
-    . c 1 b b b 1 1 1 b b b 1 c . 
-    c 1 b b b 1 b b b 1 b b c 1 c 
-    c 1 b b b 1 b b b 1 b b c 1 c 
-    c 1 b b b 1 1 1 1 1 b b c 1 c 
-    c 1 b b b 1 b b b 1 b b c 1 c 
-    c 1 b b b 1 b b b 1 b c c 1 c 
-    . c 1 b b b b b b b b c 1 c . 
-    . c 1 b b b b b b b c c 1 c . 
-    . . c 1 1 c c c c c 1 1 c . . 
-    . . . c c 1 1 1 1 1 c c . . . 
-    . . . . . c c c c c . . . . . 
-    `],
+assets.animation`A Button`,
 500,
 true
 )
